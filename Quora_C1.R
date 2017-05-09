ceca28d1e66c44704a2feb92da78990607cbe280
JediPro <pratikranit@gmail.com>
2017-05-09 23:49:07 +0530

    Adding Cosine Similarity

--- Quora_C1.R
+++ Quora_C1.R
@@ -8,8 +8,10 @@ library(caret)
 library(tm)
 library(tokenizers)
 library(RWeka)
+library(lsa)
 library(Metrics)
 library(slam)
+library(MASS)
 library(car)
 
 # Process text ####
@@ -76,49 +78,58 @@ Qall = rbind(Q1data, Q2data) # combine all questions into one vector
 Qtext = Corpus(VectorSource(Qall$text))
 
 # Form Term Doc Mat with Tf IDf weights
-TermDocMat1 = TermDocumentMatrix(Qtext, 
-                                 control = list(weighting = function(x) weightTfIdf(x,  normalize = T)))
-TDM.Q1 = TermDocMat1[,1:nrow(dtrain)] # extract weights for Q1
-TDM.Q2 = TermDocMat1[,(nrow(dtrain)+1):nrow(Qall)] # extract weights for Q2
+DocTermMat1 = DocumentTermMatrix(Qtext, 
+                                 control = list(weighting = function(x) 
+                                   weightTfIdf(x,  normalize = T)))
+DTM.Q1 = DocTermMat1[1:nrow(dtrain),] # extract weights for Q1
+DTM.Q2 = DocTermMat1[(nrow(dtrain)+1):nrow(Qall),] # extract weights for Q2
 
-TDM.colsum.Q1 = rollup(TDM.Q1, 1, FUN=sum, REDUCE = F) # sum of weights in question set 1
-TDM.colsum.Q2 = rollup(TDM.Q2, 1, FUN=sum, REDUCE = F) #sum of weights in question set 1
+DTM.colsum.Q1 = rollup(DTM.Q1, 2, FUN=sum, REDUCE = F) # sum of weights in question set 1
+DTM.colsum.Q2 = rollup(DTM.Q2, 2, FUN=sum, REDUCE = F) #sum of weights in question set 1
 
-dtrain$Q1TFIDF = as.vector(TDM.colsum.Q1[1,]) # TFIDF weights of Q1
-dtrain$Q2TFIDF = as.vector(TDM.colsum.Q2[1,]) # weights of Q2
+dtrain$Q1TFIDF = as.vector(DTM.colsum.Q1[1,]) # TFIDF weights of Q1
+dtrain$Q2TFIDF = as.vector(DTM.colsum.Q2[1,]) # weights of Q2
 
-TDM.mul = TDM.Q1 * TDM.Q2 # Multiply TDM for both Q sets to get weights of common terms
-TDM.mul = sqrt(TDM.mul) # normalize after the multliplication
+DTM.mul = DTM.Q1 * DTM.Q2 # Multiply DTM for both Q sets to get weights of common terms
+DTM.mul = sqrt(DTM.mul) # normalize after the multliplication
 
-TDM.colsum = rollup(TDM.mul, 1, FUN=sum, REDUCE = FALSE) # sum of weights in each doc
-dtrain$CommonTFIDF = as.vector(TDM.colsum[1,]) # load to training data frame
+DTM.colsum = rollup(DTM.mul, 2, FUN=sum, REDUCE = FALSE) # sum of weights in each doc
+dtrain$CommonTFIDF = as.vector(DTM.colsum[1,]) # load to training data frame
+
+# create function to calculate cosine distance
+cosineDist = function(A, B){
+  row_sums(A * B) / sqrt(row_sums(A * A) * row_sums(B * B))
+}
+DTM.cosine = cosineDist(DTM.Q1, DTM.Q2) # compute cosine distance for each pair
+dtrain$CosineSimil = DTM.cosine
+dtrain$CosineSimil = recode(dtrain$CosineSimil, "NA=0")
 
 # Find TF-IDF manually
-TermDocMat2 = TermDocumentMatrix(Qtext) # Create TDM using TF
-TDM2.termOccur = rollup(TermDocMat2, 2, FUN=length) # no. of docs containing respective term
-term.idf = log(nrow(Qall)/TDM2.termOccur$v) # IDF for each term
+DocTermMat2 = DocumentTermMatrix(Qtext) # Create DTM using TF
+DTM2.termOccur = rollup(DocTermMat2, 1, FUN=length) # no. of docs containing respective term
+term.idf = log(nrow(Qall)/DTM2.termOccur$v) # IDF for each term
 
 recs = nrow(dtrain) # no. of question sets
-# split Overall TDMs
-TDM.Q1 = TermDocMat2[,1:recs]
-TDM.Q2 = TermDocMat2[,(recs+1):ncol(TermDocMat2)]
+# split Overall DTMs
+DTM.Q1 = DocTermMat2[1:recs,]
+DTM.Q2 = DocTermMat2[(recs+1):nrow(DocTermMat2),]
 
 # Find IF-TDF weights for Each question of set
-TDM.Q1.mul = crossprod_simple_triplet_matrix(TDM2.termOccur, TDM.Q1)
-TDM.Q2.mul = crossprod_simple_triplet_matrix(TDM2.termOccur, TDM.Q2)
+DTM.Q1.mul = crossprod_simple_triplet_matrix(DTM2.termOccur, DTM.Q1)
+DTM.Q2.mul = crossprod_simple_triplet_matrix(DTM2.termOccur, DTM.Q2)
 # Normalize TF-IDFs
-TDM.Q1.mul = TDM.Q1.mul/(recs*2)
-TDM.Q2.mul = TDM.Q2.mul/(recs*2)
+DTM.Q1.mul = DTM.Q1.mul/(recs*2)
+DTM.Q2.mul = DTM.Q2.mul/(recs*2)
 # Append to data frame
-dtrain$Q1IDF = TDM.Q1.mul[1,]
-dtrain$Q2IDF = TDM.Q2.mul[1,]
+dtrain$Q1IDF = DTM.Q1.mul[1,]
+dtrain$Q2IDF = DTM.Q2.mul[1,]
 
 # Find TF-IDFs of common terms
-TDM.Mul = TDM.Q1 * TDM.Q2 # Multiply TDMs to keep only common terms in each sets
-TDM.Mul = sqrt(TDM.Mul) # Take square root to remove multiplication effect
-TDM.mul2 = crossprod_simple_triplet_matrix(TDM2.termOccur, TDM.Mul) # Multiply to calc common word weight
-TDM.mul2 = TDM.mul2/(recs*2) # Divide by number of all docs to normalize
-dtrain$IDF = TDM.mul2[1,] # append to original file
+DTM.Mul = DTM.Q1 * DTM.Q2 # Multiply DTMs to keep only common terms in each sets
+DTM.Mul = sqrt(DTM.Mul) # Take square root to remove multiplication effect
+DTM.mul2 = crossprod_simple_triplet_matrix(DTM2.termOccur, DTM.Mul) # Multiply to calc common word weight
+DTM.mul2 = DTM.mul2/(recs*2) # Divide by number of all docs to normalize
+dtrain$IDF = DTM.mul2[1,] # append to original file
 
 # Similarity of IDF scores
 dtrain$IDFQuotient = apply(dtrain[,names(dtrain) %in% c('Q1IDF','Q2IDF')], 1, function(x) {min(x)/max(x)})
@@ -126,8 +137,9 @@ dtrain$IDFQuotient = recode(dtrain$IDFQuotient, "NA=0")
 
 # Split data ####
 # vector of selected predictors
-preds.all = c("CharQuotient","WordQuotient","CommonQuotient","IDF","Q1IDF","Q2IDF","IDFQuotient",
-              "Q1TFIDF","Q2TFIDF","CommonTFIDF")
+#preds.all = c("CharQuotient","WordQuotient","CommonQuotient","IDF","Q1IDF","Q2IDF","IDFQuotient",
+#             "Q1TFIDF","Q2TFIDF","CommonTFIDF","CosineSimil")
+preds.all = c("WordQuotient","CommonQuotient","Q1TFIDF","Q2TFIDF","CommonTFIDF","CosineSimil")
 # create design matrix
 mtrain = model.matrix(~., data = dtrain[,names(dtrain) %in% preds.all])
 mtrain = as.data.frame(mtrain)
@@ -137,6 +149,7 @@ mtrain = mtrain[,-1]
 mtrain.max = apply(X = mtrain, MARGIN = 2, FUN = max)
 mtrain.min = apply(X = mtrain, MARGIN = 2, FUN = min)
 mtrain = apply(X = mtrain, MARGIN = 2, FUN = function(x) {(x - min(x))/(max(x)-min(x))})
+mtrain = as.data.frame(mtrain)
 
 # bring positive class to around 17% of total data
 mtrain.negclass = mtrain[which(dtrain$is_duplicate==0),]
@@ -155,7 +168,14 @@ resp.t = resp[prt]
 resp.v = resp[-prt]
 
 # Fit models ####
-preds = c("CharQuotient","WordQuotient","CommonQuotient","IDF","Q1IDF","Q2IDF","IDFQuotient")
+
+modlr.full = glm(resp.t~., data = dt, family = "binomial")
+modlr.null = glm(resp.t~1, data = dt, family = "binomial")
+modlr.step = step(object = modlr.null, 
+                  scope = list(lower=formula(modlr.null), upper=formula(modlr.full)),
+                  direction = "both", trace = 1)
+modlr.step = stepAIC(modlr.full, direction = "both", scope = )
+
 # linear regression
 set.seed(713)
 cvCtrl=trainControl(method = "repeatedcv", number=3, repeats = 3, verboseIter = T)
@@ -167,18 +187,18 @@ logLoss(predicted = as.numeric(modlr.p.1), actual = resp.v) # 0.37404
 # XGboost
 set.seed(713)
 cvCtrl=trainControl(method = "repeatedcv", number=2, repeats = 2, verboseIter = T)
-modxgbl=train(x = dt, y = as.factor(resp.t), method = "xgbTree", trControl = cvCtrl, tuneLength = 3)
+modxgbl=train(x = dt, y = as.factor(resp.t),method = "xgbTree", trControl = cvCtrl,tuneLength = 3)
 modxgbl.p=predict(modxgbl, newdata = dv, type = "prob")
 modxgbl.p.1=modxgbl.p$`1`
 logLoss(predicted = as.numeric(modxgbl.p.1), actual = resp.v) #0.329
 
-# Random forest
+# Neural network
 set.seed(713)
-cvCtrl=trainControl(method = "repeatedcv", number=2, repeats = 2, verboseIter = T)
-modrf = train(x = dt, y = as.factor(resp.t), method = "rf", trControl = cvCtrl, tuneLength = 3)
-modrf.p=predict(modrf, newdata = dv, type = "prob")
-modrf.p.1=modrf.p$`1`
-logLoss(predicted = as.numeric(modrf.p.1), actual = resp.v)
+cvCtrl=trainControl(method = "repeatedcv", number=3, repeats = 3, verboseIter = T)
+modnn = train(x = dt, y = as.factor(resp.t), method = "nnet", trControl = cvCtrl, tuneLength = 3)
+modnn.p=predict(modnn, newdata = dv, type = "prob")
+modnn.p.1=modnn.p$`1`
+logLoss(predicted = as.numeric(modnn.p.1), actual = resp.v) #0.3468
 
 # Process Test file ####
 dtest = read.csv("test.csv", header = T, sep = ",")
@@ -241,52 +261,59 @@ Qall.test = rbind(Q1data.test, Q2data.test) # combine all questions into one vec
 Qtext.test = Corpus(VectorSource(Qall.test$text.test))
 
 # Form Term Doc Mat with Tf IDf weights
-TermDocMat1 = TermDocumentMatrix(Qtext.test, 
-                                 control = list(weighting = function(x) weightTfIdf(x,  normalize = T)))
-TDM.Q1 = TermDocMat1[,1:nrow(dtest)] # extract weights for Q1
-TDM.Q2 = TermDocMat1[,(nrow(dtest)+1):nrow(Qall.test)] # extract weights for Q2
+DocTermMat1 = DocumentTermMatrix(Qtext.test, 
+                        control = list(weighting = function(x) weightTfIdf(x,  normalize = T)))
+DTM.Q1 = DocTermMat1[1:nrow(dtest),] # extract weights for Q1
+DTM.Q2 = DocTermMat1[(nrow(dtest)+1):nrow(Qall.test),] # extract weights for Q2
 
-TDM.colsum.Q1 = rollup(TDM.Q1, 1, FUN=sum, REDUCE = F) # sum of weights in question set 1
-TDM.colsum.Q2 = rollup(TDM.Q2, 1, FUN=sum, REDUCE = F) #sum of weights in question set 1
+DTM.colsum.Q1 = rollup(DTM.Q1, 2, FUN=sum, REDUCE = F) # sum of weights in question set 1
+DTM.colsum.Q2 = rollup(DTM.Q2, 2, FUN=sum, REDUCE = F) #sum of weights in question set 1
 
-dtest$Q1TFIDF = as.vector(TDM.colsum.Q1[1,]) # TFIDF weights of Q1
-dtest$Q2TFIDF = as.vector(TDM.colsum.Q2[1,]) # weights of Q2
+dtest$Q1TFIDF = as.vector(DTM.colsum.Q1[1,]) # TFIDF weights of Q1
+dtest$Q2TFIDF = as.vector(DTM.colsum.Q2[1,]) # weights of Q2
 
-TDM.mul = TDM.Q1 * TDM.Q2 # Multiply TDM for both Q sets to get weights of common terms
-TDM.mul = sqrt(TDM.mul) # normalize after the multliplication
+DTM.mul = DTM.Q1 * DTM.Q2 # Multiply DTM for both Q sets to get weights of common terms
+DTM.mul = sqrt(DTM.mul) # normalize after the multliplication
 
-TDM.colsum = rollup(TDM.mul, 1, FUN=sum, REDUCE = FALSE) # sum of weights in each doc
-dtest$CommonTFIDF = as.vector(TDM.colsum[1,]) # load to testing data frame
+DTM.colsum = rollup(DTM.mul, 2, FUN=sum, REDUCE = FALSE) # sum of weights in each doc
+dtest$CommonTFIDF = as.vector(DTM.colsum[1,]) # load to testing data frame
+
+# create function to calculate cosine distance
+DTM.cosine = cosineDist(DTM.Q1, DTM.Q2) # compute cosine distance for each pair
+dtest$CosineSimil = DTM.cosine
+dtest$CosineSimil = recode(dtest$CosineSimil, "NA=0")
 
 # Find TF-IDF manually
-TermDocMat2.test = TermDocumentMatrix(Qtext.test) # Create TDM using TF
-TDM2.termOccur.test = rollup(TermDocMat2.test, 2, FUN=length) # no. of docs containing respective term
-term.idf.test = log(nrow(Qall.test)/TDM2.termOccur.test$v) # IDF for each term
+DocTermMat2.test = DocumentTermMatrix(Qtext.test) # Create DTM using TF
+DTM2.termOccur.test = rollup(DocTermMat2.test, 2, FUN=length) #no. of docs having respective term
+term.idf.test = log(nrow(Qall.test)/DTM2.termOccur.test$v) # IDF for each term
 
 recs = nrow(dtest) # no. of question sets
-# split Overall TDMs
-TDM.Q1 = TermDocMat2.test[,1:recs]
-TDM.Q2 = TermDocMat2.test[,(recs+1):ncol(TermDocMat2.test)]
+# split Overall DTMs
+DTM.Q1 = DocTermMat2.test[1:recs,]
+DTM.Q2 = DocTermMat2.test[(recs+1):ncol(DocTermMat2.test),]
 
 # Find IF-TDF weights for Each question of set
-TDM.Q1.mul = crossprod_simple_triplet_matrix(TDM2.termOccur.test, TDM.Q1)
-TDM.Q2.mul = crossprod_simple_triplet_matrix(TDM2.termOccur.test, TDM.Q2)
+DTM.Q1.mul = crossprod_simple_triplet_matrix(DTM2.termOccur.test, DTM.Q1)
+DTM.Q2.mul = crossprod_simple_triplet_matrix(DTM2.termOccur.test, DTM.Q2)
 # Normalize TF-IDFs
-TDM.Q1.mul = TDM.Q1.mul/(recs*2)
-TDM.Q2.mul = TDM.Q2.mul/(recs*2)
+DTM.Q1.mul = DTM.Q1.mul/(recs*2)
+DTM.Q2.mul = DTM.Q2.mul/(recs*2)
 # Append to data frame
-dtest$Q1IDF = TDM.Q1.mul[1,]
-dtest$Q2IDF = TDM.Q2.mul[1,]
+dtest$Q1IDF = DTM.Q1.mul[1,]
+dtest$Q2IDF = DTM.Q2.mul[1,]
 
 # Find TF-IDFs of common terms
-TDM.Mul = TDM.Q1 * TDM.Q2 # Multiply TDMs to keep only common terms in each sets
-TDM.Mul = sqrt(TDM.Mul) # Take square root to remove multiplication effect
-TDM.mul2 = crossprod_simple_triplet_matrix(TDM2.termOccur.test, TDM.Mul) # Multiply for common word weight
-TDM.mul2 = TDM.mul2/(recs*2) # Divide by number of all docs to normalize
-dtest$IDF = TDM.mul2[1,] # append to original file
+DTM.Mul = DTM.Q1 * DTM.Q2 # Multiply DTMs to keep only common terms in each sets
+DTM.Mul = sqrt(DTM.Mul) # Take square root to remove multiplication effect
+# Multiply for common word weight
+DTM.mul2 = crossprod_simple_triplet_matrix(DTM2.termOccur.test, DTM.Mul) 
+DTM.mul2 = DTM.mul2/(recs*2) # Divide by number of all docs to normalize
+dtest$IDF = DTM.mul2[1,] # append to original file
 
 # Similarity of IDF scores
-dtest$IDFQuotient = apply(dtest[,names(dtest) %in% c('Q1IDF','Q2IDF')], 1, function(x) {min(x)/max(x)})
+dtest$IDFQuotient = apply(dtest[,names(dtest) %in% c('Q1IDF','Q2IDF')], 1, 
+                          function(x) {min(x)/max(x)})
 dtest$IDFQuotient = recode(dtest$IDFQuotient, "NA=0")
 
 # Predict test scores ####
@@ -299,10 +326,11 @@ mtest = mtest[,-1]
 # standardize values
 mtest = sweep(x = mtest, MARGIN = 2, STATS = mtrain.max, FUN = "/")
 
-mod.test=predict(modxgbl, newdata = mtest, type = "prob") # predict on test set
+mod.test=predict(modlr.full, newdata = mtest, type = "response") # predict on test set
 mod.test.1=mod.test$`1` # choose probabilities for positive class
 
 # Create submission
 dsub = read.csv("sample_submission.csv", header = T, sep = ",")
-dsub$is_duplicate = mod.test.1
+dsub$is_duplicate = mod.test
 write.csv(dsub, "Dsub.csv", row.names = F) #0.438 on LB
+  
